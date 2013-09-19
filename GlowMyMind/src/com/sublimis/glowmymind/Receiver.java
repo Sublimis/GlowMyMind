54e12bd5490c07ad8712155e73da6d9ef96a2c6d
sublimis <contact@sonichandy.com>
2013-09-19 18:19:58 +0200

    added license

--- GlowMyMind/src/com/sublimis/glowmymind/Receiver.java
+++ GlowMyMind/src/com/sublimis/glowmymind/Receiver.java
@@ -1,3 +1,22 @@
+/*
+    Copyright 2013. Sublimis Solutions
+
+    This file is part of GlowMyMind.
+
+    GlowMyMind is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The software is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this software. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.sublimis.glowmymind;
 
 import com.sublimis.glowmymind.MyPreference;
